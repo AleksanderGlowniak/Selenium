b0f4316d8db69db5d449ef7235ce37ebe09476b0
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-08-13 19:54:15 +0200

    SeleniumTest

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,15 +1,40 @@
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
     @Test
     public void openGooglePage(){
-        String path = "C:\\Users\\olo88\\Desktop\\Selenium\\drivery\\geckodriver.exe";
-        System.setProperty("webdriver.gecko.driver", path);
-        WebDriver driver = new FirefoxDriver();
+        // String path = "C:\\Users\\olo88\\Desktop\\Selenium\\drivery\\geckodriver.exe";
+       /* String path = "C:\\Users\\olo88\\Desktop\\Selenium\\drivery\\IEDriverServer.exe";
+        System.setProperty("webdriver.ie.driver", path);
+        InternetExplorerOptions options = new InternetExplorerOptions();
+        options.withInitialBrowserUrl("https://www.google.pl/");*/
+        WebDriver driver = getDriver("chromek");
         driver.get("https://www.google.pl/");
+    }
+
+    public WebDriver getDriver(String browser){
+       switch (browser){
+           case "chrome":
+               String chromePath = "C:\\Users\\olo88\\Desktop\\Selenium\\drivery\\chromedriver.exe";
+               System.setProperty("webdriver.chrome.driver", chromePath);
+               return new ChromeDriver();
+           case "firefox":
+               String firefoxPath = "C:\\Users\\olo88\\Desktop\\Selenium\\drivery\\geckodriver.exe";
+               System.setProperty("webdriver.gecko.driver", firefoxPath);
+               return new FirefoxDriver();
+           case "ie":
+               String iePath = "C:\\Users\\olo88\\Desktop\\Selenium\\drivery\\IEDriverServer.exe";
+               System.setProperty("webdriver.ie.driver", iePath);
+               return new InternetExplorerDriver();
+           default:
+               throw new InvalidArgumentException("InvalidBrowserName");
+       }
     }
 }
