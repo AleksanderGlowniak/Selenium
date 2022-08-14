2e39627cfeda039bcd8652e06f8f8fe5ff7ddfd2
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-08-14 13:39:05 +0200

    WebDriverManagerTest

--- src/test/java/WebDriverManagerTest.java
+++ src/test/java/WebDriverManagerTest.java
@@ -1,8 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.managers.OperaDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.opera.OperaDriver;
 import org.testng.annotations.Test;
 
@@ -10,8 +15,14 @@ public class WebDriverManagerTest {
 
     @Test
     public void openBrowser() {
-        WebDriverManager.iedriver().setup();
-        WebDriver driver = new InternetExplorerDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+        WebDriver driver = new ChromeDriver(options);
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("alert('Hello')");
+        driver.get("https://www.wp.pl");
 
     }
 }
