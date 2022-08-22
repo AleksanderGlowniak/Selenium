2e2992bb9ce3e17803a79e858431c0a86da79cfc
AleksanderGlowniak <aleksander-glowniak@wp.pl>
2022-08-22 17:30:39 +0200

    CssSelectorTest

--- src/test/java/CssSelectorsTest.java
+++ src/test/java/CssSelectorsTest.java
@@ -27,5 +27,35 @@ public class CssSelectorsTest {
         By cssClass2 = By.cssSelector("[class='topSecret']");
         driver.findElement(cssClass2);
 
+        By all = By.cssSelector("*");
+        driver.findElement(all);
+
+        By ulInsideDiv = By.cssSelector("div ul");
+        By trInTable = By.cssSelector("table tr");
+        By trInBody = By.cssSelector("tbody tr");
+        driver.findElement(trInTable);
+        driver.findElement(trInBody);
+
+        By firstChildUlInDiv = By.cssSelector("div > ul");
+        By firstChildTrInTbody = By.cssSelector("tbody > tr");
+        driver.findElement(firstChildUlInDiv);
+        driver.findElement(firstChildTrInTbody);
+
+        By firstFormAfterLabel = By.cssSelector("label + form");
+        By allFromsAfterLabel = By.cssSelector("label ~ form");
+        driver.findElement(firstFormAfterLabel);
+        driver.findElement(allFromsAfterLabel);
+
+        By attrTag = By.cssSelector("input[name='fname']");
+        By attrContains = By.cssSelector("[name*='name']"); // zawiera ciąg znakow name
+        By attrStarts = By.cssSelector("[name^='f']"); // wartość atrybutu rozpoczyna sie od f
+        By attrEnd = By.cssSelector("[name$='name']"); // wartość atrybutu name kończy sie na ame
+
+        driver.findElement(attrTag);
+        driver.findElement(attrContains);
+        driver.findElement(attrStarts);
+        driver.findElement(attrEnd);
+
+
     }
 }
